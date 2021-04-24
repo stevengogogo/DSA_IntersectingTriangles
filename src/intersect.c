afc825153fbc71e50ffa47774b7b9f0d47fad0bf
Steven Shao-Ting Chiu <stevengogogo4321@gmail.com>
2021-04-24 18:01:05 +0800

    add docstring

--- src/intersect.c
+++ src/intersect.c
@@ -2,6 +2,7 @@
 
 
 int get_intersects(int n, int* p, int* q, int* r){
+    
     return q[0];
 }
 
--- src/intersect.h
+++ src/intersect.h
@@ -3,7 +3,17 @@
 
 #include "utils.h"
 
-
+/**
+ * @brief Get the intersects object
+ * 
+ * @param n number of points
+ * @param p array of `p` points
+ * @param q array of `q` points
+ * @param r array of `r` points
+ * @return int number of intersects
+ */
 int get_intersects(int n, int* p, int* q, int* r);
+
+
 
 #endif
