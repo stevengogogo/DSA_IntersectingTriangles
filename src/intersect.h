1515036fcb638111dafbaea285df5dce90809c1d
stevengogogo <stevengogogo4321@gmail.com>
2021-05-01 21:14:36 +0800

    Register Doc

--- src/intersect.h
+++ src/intersect.h
@@ -1,3 +1,13 @@
+/**
+ * @file intersect.h
+ * @author Shao-Ting Chiu 
+ * @brief Calculating inversions
+ * @version 0.1
+ * @date 2021-05-01
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 #ifndef INTERSECT_H
 #define INTERSECT_H
 
@@ -33,6 +43,13 @@ void kill_path(Paths);
 void MERGE_SORT_FIRSTofThree(int* A, int* a2, int* a3, int p, int r);
 /** Merge process of @ref MERGE_SORT_FIRSTofThree */
 void MERGE_FIRSTofThree(int* A, int* a2, int* a3, int p, int q, int r);
+
+/** Merge sort and total count inversions
+ * @note Definition of inversions:
+ * 
+*/
+int MERGE_SORT_COUNT_INVERSION(int* Ps, int* Ls, int* Rs,int l, int r);
+int MERGE_COUNT_INVERSION(int* Ps, int* Ls, int* Rs, int l, int q, int r);
 
 /** Sort ps in paths. Paths.l and Path.r will keep paired with Paths.p*/
 void sortPaths_P(Paths);
