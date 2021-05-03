58322a2f636e4c0aaf48475d8cd22f351206bda3
stevengogogo <stevengogogo4321@gmail.com>
2021-05-04 02:35:56 +0800

    change data gen

--- test/genData.sh
+++ test/genData.sh
@@ -3,6 +3,6 @@
 echo "generate data"
 
 # Path Length Number_files 
-/home/ubuntu/.local/bin/julia test/dataGen.jl test/dataGen 100000 1
+/home/ubuntu/.local/bin/julia test/dataGen.jl test/dataGen 4000 1
 
 echo "Complete"
